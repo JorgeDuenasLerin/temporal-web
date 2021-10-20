65a3ec3159582d48acd16ab500e4792fb25541b6
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-10-20 13:53:29 +0200

    Introducido PHP

--- web01/index.php
+++ web01/index.php
@@ -7,7 +7,14 @@
     <script src="js/prog.js"></script>
   </head>
   <body>
+    <pre>
+      Ejecuta php -S localhost:9000
+      visital con tu navegador
+    </pre>
     <h1>Esto es un título</h1>
     <p>Esto es un párrafo</p>
+    <?php
+      echo "Esto lo escribe <b>PHP</b>"
+    ?>
   </body>
 </html>
